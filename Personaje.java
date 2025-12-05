df3d158fc21ee4524afd8ac8fb4e64429a97314b
mohamedboukoba00-web <your_email@example.com>
2025-12-05 09:23:47 +0100

    modificacion de commit

--- Personaje.java
+++ Personaje.java
@@ -1,15 +1,37 @@
 public class Personaje {
- private String nom;
- private int dinero;
- public Personaje(String nom, int dinero) {
- this.nom = nom;
- this.dinero = dinero;
- }
- public void mostrarInfo() {
- System.out.println(nom + " tiene " + dinero + "$.");
- }
- public static void main(String[] args) {
- Personaje jugador = new Personaje("Trevor", 500);
- jugador.mostrarInfo();
- }
+    private String nom;
+    private int dinero;
+
+    public Personaje(String nom, int dinero) {
+        this.nom = nom;
+        this.dinero = dinero;
+    }
+
+    // GETTERS
+    public String getNombre() {
+        return nom;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    // SETTER
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
+
+    public void mostrarInfo() {
+        System.out.println(nom + " tiene " + dinero + "$.");
+    }
+
+    public static void main(String[] args) {
+        Personaje jugador = new Personaje("Trevor", 500);
+        jugador.mostrarInfo();
+
+        // Aquí usamos la función de gastar dinero
+        GestionDinero.gastarDinero(jugador, 100);
+
+        jugador.mostrarInfo();
+    }
 }
